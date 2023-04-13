b7fab18198d418763b87261a6bf71cb622b46cf5
Umama Masroor <umamafatima319@gmail.com>
2023-04-13 11:00:22 -0700

    practical # 4

---
+++ practical # 4/practical4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int day1, day2, day3, day4, day5, day6;
+
+     printf("Enter Temperature of day 1: ");
+    scanf("%d", &day1);
+
+      printf("Enter Temperature of day 2:");
+    scanf("%d",&day2);
+
+      printf("Enter Temperature of day 3:");
+    scanf("%d",&day3);
+
+      printf("Enter Temperature of day 4:");
+    scanf("%d",&day4);
+
+      printf("Enter Temperature of day 5:");
+    scanf("%d",&day5);
+
+     printf("Enter Temperature of day 6:");
+    scanf("%d",&day6);
+    
+    printf("\n\n\t\t***Temperature Of Six Days***\n\n");
+    printf("Temperature of day 1 is %d\n", day1);
+    printf("Temperature of day 2 is %d\n", day2);
+    printf("Temperature of day 3 is %d\n", day3);
+    printf("Temperature of day 4 is %d\n", day4);
+    printf("Temperature of day 5 is %d\n", day5);
+    printf("Temperature of day 6 is %d\n", day6);
+
+return 0;
+}
